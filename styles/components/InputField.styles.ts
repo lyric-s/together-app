28f63468529dada2d77039e26bbc891020f57a64
Mromellete <mohamedam555555555@gmail.com>
2025-11-20 22:22:04 +0100

    adding component Inputfield for web and mobile

---
+++ styles/components/InputField.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from '@/constants/colors';
+
+export default StyleSheet.create({
+    field: {
+        width: '75%',
+        alignSelf: 'center',
+        backgroundColor: Colors.violet,
+        borderRadius: 8,
+        height: 48,
+        paddingHorizontal: 24,
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        marginTop: 12,
+    },
+});
