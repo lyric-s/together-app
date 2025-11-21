01e54fdf3117d8e77eb4a324dad308d35d38a414
Lucioleee <purplelucie94@gmail.com>
2025-11-21 22:59:55 +0100

    [TA-65] Category label C13

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Connexion from '@/pages/Connexion';
+import CustomButton from '@/components/ImageButton'
 
 export default function Index() {
-    return <Connexion />;
+    return <CustomButton
+            image={require("../assets/images/reward.png")}
+            onPress={() => console.log("clic !")}
+            />;
 }
