25a2fdd3f3d339aef29ab9bc88bb5a80e91b7c9e
Lucioleee <147144033+Lucioleee@users.noreply.github.com>
2025-11-21 23:50:07 +0100

    [TA-65] c13 category label (#18)
    * feature/TA-69-C17-search-bar
    
    * feature/TA-69-C17-search-bar colors modifications
    
    * [TA-65] Category label C13

---
+++ components/CategoryLabel.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Text, View, StyleSheet} from "react-native";
+import { Colors } from "../constants/colors";
+
+// -------- component C13 Category Label
+type Props = {
+  text: string;
+  backgroundColor: string;      
+};
+
+export default function CategoryLabel({
+  text,
+  backgroundColor,
+}: Props) {
+  return (
+    <View
+      style={[styles.container, { backgroundColor }]}
+    >
+      <Text style={[styles.text]} numberOfLines={1} ellipsizeMode="tail">
+        {text}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderRadius: 12,
+    margin: 10,
+    alignSelf: "flex-start",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: Colors.black, 
+    fontSize: 14,
+    fontWeight: "600",
+  },
+});
