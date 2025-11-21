6ce73bcce5debca9728d9973b60c46816580a80c
irene <144844410+irenex30@users.noreply.github.com>
2025-11-21 19:58:57 +0100

    TA-42 The main page here is Modification Mission

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,3 @@
-/*
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import ChangeMission from './pages_web/assos/ChangeMission';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <ChangeMission/>
-      */
-      {/*
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      */}
-      {/*
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
-*/}
 
 import { Stack } from 'expo-router';
 import React from 'react';
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+//import Connexion from '@/pages/Connexion';
 import ChangeMission from './pages_web/assos/ChangeMission';
 
 export default function Index() {
+    //return <Connexion />;
     return <ChangeMission />;
 }
