dd3d6915e34be66d3a49dd91232ff0684615bb65
Lyric's <160358927+Lyric-s@users.noreply.github.com>
2025-11-28 15:49:41 +0100

    Revert "ajout des dossiers pages et styles et rectification"
    This reverts commit e0f68b79da4ed65e5c23ddfd1e688e9bfbeabc3d.

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,6 +1,7 @@
 export const Colors = {
     orange: '#FF9A66',
     violet: '#6A66B8',
+<<<<<<< HEAD
     topBackground: '#FFF7F3',
     bottomBackground: '#FFE5CF',
     violetText: '#544FB3',
@@ -15,5 +16,12 @@ export const Colors = {
     red: '#FF2626',
     buttonBackgroundViolet: '#584EAF',
     whiteLittleGray: "#DDDDDD"
-
+=======
+    fondHaut: '#FFF7F3',
+    fondBas: '#FFE5CF',
+    texteViolet: '#544FB3',
+    texteGris: '#6B6475',
+    champFond: '#6A66B8',
+    champBlanc: '#FFFFFF',
+>>>>>>> 3ba22b2d (adding component  button for web and mobile)
 };
