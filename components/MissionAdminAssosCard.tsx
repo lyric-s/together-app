65476f8ce28fc5f7753cf38c60c390abd383d2e0
Lucioleee <purplelucie94@gmail.com>
2025-11-28 19:00:21 +0100

    docs: TA-66 adding docstring

--- components/MissionAdminAssosCard.tsx
+++ components/MissionAdminAssosCard.tsx
@@ -9,7 +9,28 @@ interface MissionAdminAssosCardProps {
     image: any; // require(...)
     onPressDetail: () => void;
 }
-
+/**
+ * Renders an admin/association mission card.
+ *
+ * This card is used in the admin/association dashboard to display mission information.
+ * It shows:
+ *  - A fixed-size header image
+ *  - The mission title
+ *  - The association name
+ *  - The mission date (formatted with the "fr-FR" locale)
+ *  - A bottom-right "Voir détail" button
+ *
+ * The card is responsive and intended to be used inside a parent container
+ * with horizontal layout (row + wrap) so that multiple cards can appear side by side.
+ *
+ * @param mission_title        Title of the mission.
+ * @param association_name     Name of the association.
+ * @param date                 Mission date. The time part is shown only if hour !== 0.
+ * @param image                Image displayed at the top of the card (require(...)).
+ * @param onPressDetail        Callback executed when the "Voir détail" button is pressed.
+ *
+ * @returns A styled mission card component dedicated to admin/association interfaces.
+ */
 export default function MissionAdminAssosCard({
     mission_title,
     association_name,
