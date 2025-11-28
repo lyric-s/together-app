d2f5b24a32d79778bef8d6ae4f3fa96154f3747a
Lucioleee <purplelucie94@gmail.com>
2025-11-28 18:51:25 +0100

    feat: TA-66 mission admin assos card

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import BackButton from '@/components/BackButton';
+import {  View } from 'react-native';
+import Cross from '@/components/Cross';
+import MissionAdminAssosCard from '@/components/MissionAdminAssosCard';
 
-export default function Index() {
-    return <BackButton
-                name_page = 'Mon profil'
+export default function Connexion() {
+    return (
+        <View style={{ flex: 1 ,flexDirection: "row",     // aligne horizontalement
+    flexWrap: "wrap",}}>
+            <MissionAdminAssosCard
+            mission_title='mission a la croix '
+            association_name="Croix Rouge"
+            date={new Date()}
+            image={require("../assets/images/waste.png")}
+            onPressDetail={() => console.log("Voir détail")}
             />
+            <MissionAdminAssosCard
+            mission_title='mission a la croix '
+            association_name="Croix Rouge"
+            date={new Date()}
+            image={require("../assets/images/waste.png")}
+            onPressDetail={() => console.log("Voir détail")}
+            />
+        </View>
+    );
 }
