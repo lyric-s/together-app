0367b83e30987dc2c28b0b14d7f2bdf9b28fd34b
Lyric's <160358927+Lyric-s@users.noreply.github.com>
2025-11-28 15:56:42 +0100

    [TA-58] C6 button

---
+++ styles/components/Button.styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from '@/constants/colors';
+
+export default StyleSheet.create({
+    button: {
+        marginTop: 98,
+        alignSelf: 'center',
+        width: '56%',
+        maxWidth: 260,
+        height: 50,
+        borderRadius: 30,
+        backgroundColor: Colors.orange,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: '500',
+    },
+});
