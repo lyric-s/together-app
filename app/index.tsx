d0727c51f5b7e4f8346dd0d5f66995575c9dd342
Lyric's <160358927+Lyric-s@users.noreply.github.com>
2025-11-28 15:49:41 +0100

    Revert "adding component  button for web and mobile"
    This reverts commit 113acffb47dd0a909d1371a8aee69c85753b4211.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {  View } from 'react-native';
 import Cross from '@/components/Cross';
 
@@ -7,25 +6,6 @@ export default function Connexion() {
     return (
         <View style={{ flex: 1 }}>
             <Cross onClose={() => console.log('Fermeture')} />
-=======
-import { View } from 'react-native';
-import BoutonAuth from '@/components/Button';
-
-export default function Index() {
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: '#FDF7F2',
-            }}
-        >
-            <BoutonAuth
-                texte="Test button"
-                onPress={() => console.log('Bouton pressé')}
-            />
->>>>>>> 3ba22b2d (adding component  button for web and mobile)
         </View>
     );
 }
