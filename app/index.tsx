ef38524ae49473f687bfaf83313445c582f89a6a
Lucioleee <purplelucie94@gmail.com>
2025-11-30 00:02:09 +0100

    feat: TA-72 mobile search bar using api for city filter

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,39 @@
-import { View } from 'react-native';
-import MissionVolunteerCard from '@/components/MissionVolunteerCard';
+import React from 'react';
+import BackButton from '@/components/BackButton';
+import { View, Text } from 'react-native';
+import MobileSearchBar from '@/components/MobileSearchBar';
+import { useState } from "react";
+
 
 export default function Index() {
+    const [results, setResults] = useState([]);
+  
+  const categoryList = [
+    "Environnement",
+    "Social",
+    "Éducation",
+    "Culture",
+    "Animaux",
+  ];
 
-    return (
-        <View style={{ flex: 1 ,flexDirection: "row",     // aligne horizontalement
-        flexWrap: "wrap", alignItems: 'center'}}>
-            <MissionVolunteerCard
-            mission_title="Nettoyage de la plage"
-            association_name="Ocean Protect"
-            city="Marseille"
-            date={new Date("2025-04-23T09:00:00")}
-            number_max_volunteers={20}
-            number_of_volunteers={7}
-            favorite={false}
-            category_label="Nature"
-            category_color="red"
-            image={require("../assets/images/dogs_img.png")}
-            onPressMission={() => console.log("Mission 1 clicked")}
-            onPressFavorite={(fav) => console.log("Favorite 1:", fav)}
-        />
+  const defaultCity = "Paris";
 
-        </View>
-    );
+  const handleSearch = (searchText: string, filters: any) => {
+    console.log("Recherche lancée !");
+    console.log("Texte :", searchText);
+    console.log("Filtres :", filters);
+
+  };
+
+  return (
+    <View style={{ flex: 1, padding: 20 }}>
+      
+      <MobileSearchBar
+        category_list={categoryList}
+        default_city={defaultCity}   // option
+        onSearch={handleSearch}
+      />
+
+    </View>
+  );
 }
