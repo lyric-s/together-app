a56b03f96800f3fd7a46a9e77267ba3a96207623
irene <144844410+irenex30@users.noreply.github.com>
2025-12-07 20:22:51 +0100

    fix: TA-42 add Mission type

---
+++ types/Mission.ts
@@ -0,0 +1,13 @@
+export type Mission = {
+  id: string,
+  title: string,
+  image: string,
+  dateStart: string,
+  dateEnd: string,
+  categ: string,
+  nbMin: number | string,
+  nbMax: number | string,
+  nbRegistered: number | string,
+  place: string,
+  description: string,
+};
