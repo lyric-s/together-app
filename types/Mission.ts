bf96edc71a12026a43aa6c0914c62ed9e0ba708e
irene <144844410+irenex30@users.noreply.github.com>
2025-12-13 01:37:14 +0100

    feat: TA-102 begin page Account Benevole

--- types/Mission.ts
+++ types/Mission.ts
@@ -6,7 +6,11 @@ export type Mission = {
   category: string;
   categoryColor: string;
   date: Date;
+<<<<<<< HEAD
   image: string;
+=======
+  image: any;
+>>>>>>> c5d54fc (feat: TA-102 begin page Account Benevole)
   number_max_volunteers: number;
   number_of_volunteers: number;
   favorite?: boolean;
