6738026b3085f1eaa6f159258d2b1bdaecef5c9b
Lucioleee <purplelucie94@gmail.com>
2025-12-13 14:26:14 +0100

    test: TA-10 sidebar + creation mission page

--- app/mission_creation.tsx
+++ app/mission_creation.tsx
@@ -1,5 +1,19 @@
+import { View } from "react-native";
 import MissionCreation from "../pages/MissionCreation";
+import Sidebar from "@/components/SideBar";
+import { useRouter } from "expo-router";
 
 export default function Page() {
-  return <MissionCreation />;
+  const router = useRouter();
+  
+  return <View style={{ flex: 1, flexDirection: "row" }}>
+
+    <Sidebar 
+    userType="association"
+     userName="Fred" 
+     onNavigate={(route) => router.push(('/' + route) as any)} 
+     
+       />
+    <MissionCreation />
+    </View>;
 }
