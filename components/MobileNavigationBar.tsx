19909f372ad7ad3d5fcacd081c197b89cc164ef4
AMK0602 <comete.night@gmail.com>
2025-12-19 19:19:13 +0100

    feat: TA-76 Adding Coderabbit modifications

--- components/MobileNavigationBar.tsx
+++ components/MobileNavigationBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity } from 'react-native';import { Ionicons } from '@expo/vector-icons';
+import { View, TouchableOpacity, StyleSheet, } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { styles } from '@/styles/components/MobileNavigationBarStyles';
 
 type TabItem = {
