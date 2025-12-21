42b159e4123ce334184322a2dd994ef5ed1e4e1d
AMK0602 <comete.night@gmail.com>
2025-12-21 17:37:38 +0100

    feat: TA-76 Adding handlePress function

--- components/MobileNavigationBar.tsx
+++ components/MobileNavigationBar.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity} from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router'; // Import du router
 import { styles } from '@/styles/components/MobileNavigationBarStyles';
+
 
 type TabItem = {
     id: string;
@@ -9,15 +11,40 @@ type TabItem = {
     iconOutline: keyof typeof Ionicons.glyphMap;
 };
 
+
 const BottomNavBar: React.FC = () => {
     const [activeTab, setActiveTab] = useState<string>('home');
+    const router = useRouter(); // Hook pour la navigation
 
     const tabs: TabItem[] = [
-        { id: 'home', iconName: 'home', iconOutline: 'home-outline' },
+        { id: 'home', iconName: 'home', iconOutline: 'home-outline' }, // Assure-toi que la route /home existe, sinon c'est souvent '/'
         { id: 'search', iconName: 'search', iconOutline: 'search-outline' },
         { id: 'library', iconName: 'book', iconOutline: 'book-outline' },
         { id: 'profile', iconName: 'person', iconOutline: 'person-outline' },
     ];
+
+ 
+    const handlePress = (tabId: string) => {
+        setActiveTab(tabId);
+
+        
+        switch (tabId) {
+            case 'home':
+                router.push('/');
+                break;
+            case 'search':
+                router.push('/recherche');
+                break;
+            case 'library':
+                router.push('annonces'); 
+                break;
+            case 'profile':
+                router.push('/profil');
+                break;
+            default:
+                break;
+        }
+    };
 
     return (
         <View style={styles.navBar}>
@@ -28,10 +55,10 @@ const BottomNavBar: React.FC = () => {
                     <TouchableOpacity
                         key={tab.id}
                         style={styles.tabButton}
-                        onPress={() => setActiveTab(tab.id)}
+                        onPress={() => handlePress(tab.id)}
                         activeOpacity={0.7}
                         accessibilityRole="button"
-                        accessibilityLabel={`${tab.id} tab`}
+                        accessibilityLabel={`Aller à l'onglet ${tab.id}`}
                         accessibilityState={{ selected: isActive }}
                     >
                         {isActive && <View style={styles.activeIndicator} />}
@@ -47,4 +74,5 @@ const BottomNavBar: React.FC = () => {
         </View>
     );
 }
+
 export default BottomNavBar;
