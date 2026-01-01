aec45ae67037d3ec2e3de05ab1809889bc14111f
irene <144844410+irenex30@users.noreply.github.com>
2026-01-01 16:46:34 +0100

    fix: TA-102 implementation mobile navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,40 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { View, useWindowDimensions } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BottomNavBar from '@/components/MobileNavigationBar';
 import { StatusBar } from 'expo-status-bar';
+import { usePathname } from 'expo-router';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const pathname = usePathname();
+  const { width } = useWindowDimensions();
+
+  // Routes where the navbar should be hidden
+  const hideNavbarRoutes = ['/login', '/signup', '/ProfilAssos'];
+  const shouldShowNavbar = !hideNavbarRoutes.includes(pathname);
+  
+  const isMobile = width < 768;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <View style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+
+        {shouldShowNavbar && isMobile && (
+              <BottomNavBar />
+          )}
+        </View>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 
 }
