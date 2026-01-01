1b86a28c26e7a5d34c5280701ec4bf1c673c8bee
irene <144844410+irenex30@users.noreply.github.com>
2026-01-01 17:13:08 +0100

    fix: TA-102 add AlertToast, add same styling for favoris section, change name using in map

--- types/Mission.ts
+++ types/Mission.ts
@@ -6,11 +6,7 @@ export type Mission = {
   category: string;
   categoryColor: string;
   date: Date;
-<<<<<<< HEAD
   image: string;
-=======
-  image: any;
->>>>>>> c5d54fc (feat: TA-102 begin page Account Benevole)
   number_max_volunteers: number;
   number_of_volunteers: number;
   favorite?: boolean;
