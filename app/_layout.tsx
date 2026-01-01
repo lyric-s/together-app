e4aea10e31c337910f66c7aa3a0d92bce1b4985e
irene <144844410+irenex30@users.noreply.github.com>
2026-01-01 16:04:56 +0100

    feat: TA-107 remove mobile navbar when it is assos account

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { View, useWindowDimensions } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BottomNavBar from '@/components/MobileNavigationBar';
 import { StatusBar } from 'expo-status-bar';
+import { usePathname } from 'expo-router';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
@@ -15,16 +19,31 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
  */
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const pathname = usePathname();
+  const { width } = useWindowDimensions();
+
+  // Routes where the navbar should be hidden
+  const hideNavbarRoutes = ['/login', '/signup', '/ProfilAssos'];
+  const shouldShowNavbar = !hideNavbarRoutes.includes(pathname);
+  
+  const isMobile = width < 768;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+
+        {shouldShowNavbar && isMobile && (
+            <View>
+              <BottomNavBar />
+            </View>
+          )}
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 
 }
