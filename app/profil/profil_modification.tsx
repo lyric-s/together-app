d9c623c096883649216b8443dad1cb34335e584c
Lucioleee <purplelucie94@gmail.com>
2026-01-04 21:58:08 +0100

    fix: TA-100 removing mobile nav bar

--- app/profil/profil_modification.tsx
+++ app/profil/profil_modification.tsx
@@ -1,10 +1,8 @@
 import { View } from "react-native";
 import ProfilModificationPage from "../../pages/ProfilModificationPage";
-import { useRouter } from "expo-router";
 import BottomNavBar from "@/components/MobileNavigationBar";
 
 export default function Page() {
-  const router = useRouter();
   
   return <View style={{ flex: 1 }}>
 
