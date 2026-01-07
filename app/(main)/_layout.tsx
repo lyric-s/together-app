dce57b64c6d17d77904e64ff0b267fe63ef7255a
irene <144844410+irenex30@users.noreply.github.com>
2026-01-07 01:17:42 +0100

    fix: TA-115 remove opacity in the dependency array

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -9,15 +9,10 @@
  * All screens are configured without headers by default. * All screens are configured without headers by default.
  */
 
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { Platform, View, useWindowDimensions } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Platform, View } from 'react-native';
 import BottomNavBar from '@/components/MobileNavigationBar';
-import { StatusBar } from 'expo-status-bar';
-import { usePathname } from 'expo-router';
-import 'react-native-reanimated';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+
 
 /**
  * RootLayout component that serves as the top-level layout wrapper.
@@ -29,24 +24,18 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
  * @returns {JSX.Element} The themed navigation stack with status bar configuration.
  */
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
   const isMobile = Platform.OS === 'ios' || Platform.OS === 'android';
 
   return (
-    <SafeAreaProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <View style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-        </Stack>
+    <View style={{ flex: 1 }}>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    </Stack>
 
-        {isMobile && (
-              <BottomNavBar />
-          )}
-        </View>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    {isMobile && (
+      <BottomNavBar />
+    )}
+    </View>
   );
 }
