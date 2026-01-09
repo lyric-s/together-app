dc6a52ebdd3d05927d1d045f477b12f8567a25f8
Lucioleee <purplelucie94@gmail.com>
2026-01-09 20:39:29 +0100

    feat: TA-120 assos history

---
+++ styles/pages/AssosHistoryStyle.ts
@@ -0,0 +1,34 @@
+import { Colors } from '@/constants/colors';
+import {
+  StyleSheet,
+} from 'react-native';
+
+export const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: '600',
+    marginBottom: 24,
+  },
+
+  cardsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 20, 
+  },
+
+  emptyText: {
+    fontSize: 16,
+    color: Colors.black,
+    marginTop: 40,
+  },
+});
