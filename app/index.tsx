0d7b88265930841749fb627cce592d7bc0cb934c
Mromellete <mohamedam555555555@gmail.com>
2026-01-15 00:26:30 +0100

    feat: TA-89  adapting front to retrieve data from back avec les autres pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import LoginMobilePage from "@/pages/AssociationProfileVerification";
+import LoginMobilePage from "@/pages/DashboardAdmin";
 
 export default function Index() {
   return <LoginMobilePage />;
